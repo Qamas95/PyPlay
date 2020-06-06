49d47c90c9ba2c70b14831f23ad990a90a6058f1
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-06-06 16:32:54 +0200

    Open new windows and play with them.

--- calc.py
+++ calc.py
@@ -7,38 +7,18 @@ from PIL import ImageTk
 from PIL import Image
 
 root = Tk()
-root.title("Frames and stuff")
-
-# r = IntVar()
-# r.set("2")
-
-TOPPINGS = [
-    ("Pepperoni", "Pepperoni"),
-    ("Cheese", "Cheese"),
-    ("Mushroom", "Mushroom"),
-    ("Onion", "Onion"),
-]
-
-pizza = StringVar()
-pizza.set("Pepperoni")
-
-for text, topping in TOPPINGS:
-    Radiobutton(root, text=text, variable=pizza, value = topping).pack(anchor=W)
-
-def clicked(value):
-    myLabel = Label(root, text=value)
-    myLabel.pack()
-
-# Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda:clicked(r.get())).pack()
-# Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda:clicked(r.get())).pack()
+root.title('Name of app')
 
 
-# myLabel = Label(root, text=pizza.get())
-# myLabel.pack()
 
+def openWindow():
+    top = Toplevel()
+    lbl = Label(top, text="Hello").pack()
+    btn2 = Button(top, text="Close window", command = top.destroy)
+    btn2.pack()
 
-myButton = Button(root, text="Click me!", command=lambda:clicked(pizza.get()))
-myButton.pack()
+btn = Button(root, text="Open Second Window", command = openWindow)
+btn.pack()
 
 root.mainloop()
 
