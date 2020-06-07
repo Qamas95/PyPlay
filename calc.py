f96ac7e02673b07d67df599ba5ef3429d3efd0ba
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-06-07 14:58:08 +0200

    Add checkboxes and playaround with them.

--- calc.py
+++ calc.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter import filedialog
 #import this to use normal images after . like .png etc 
 #do: pip install Pillow
 #sudo apt-get install python3-pil python3-pil.imagetk
@@ -7,38 +7,23 @@ from PIL import ImageTk
 from PIL import Image
 
 root = Tk()
-root.title("Frames and stuff")
+root.title('Name of app')
+root.geometry("400x400")
 
-# r = IntVar()
-# r.set("2")
+var = StringVar()
 
-TOPPINGS = [
-    ("Pepperoni", "Pepperoni"),
-    ("Cheese", "Cheese"),
-    ("Mushroom", "Mushroom"),
-    ("Onion", "Onion"),
-]
+c = Checkbutton(root, text="Check this box", variable = var, onvalue="On", offvalue="Off")
+c.deselect()
+c.pack()
 
-pizza = StringVar()
-pizza.set("Pepperoni")
-
-for text, topping in TOPPINGS:
-    Radiobutton(root, text=text, variable=pizza, value = topping).pack(anchor=W)
-
-def clicked(value):
-    myLabel = Label(root, text=value)
-    myLabel.pack()
-
-# Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda:clicked(r.get())).pack()
-# Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda:clicked(r.get())).pack()
+def checkcheck():
+    test_label = Label(root, text=var.get()).pack()
 
 
-# myLabel = Label(root, text=pizza.get())
-# myLabel.pack()
+btn1 = Button(root, text="Click me!", command=checkcheck).pack()
 
 
-myButton = Button(root, text="Click me!", command=lambda:clicked(pizza.get()))
-myButton.pack()
+
 
 root.mainloop()
 
