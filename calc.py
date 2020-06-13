81a555b23469fe977a65c8a35b305a0584b473fa
Qamas95 <52711287+Qamas95@users.noreply.github.com>
2020-06-13 16:24:03 +0200

    Plot things in python

--- calc.py
+++ calc.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import filedialog
 import requests
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 #import this to use normal images after . like .png etc 
 #do: pip install Pillow
@@ -11,52 +13,18 @@ from PIL import Image
 from tkinter import messagebox
 
 root = Tk()
-root.title('Weather App')
+root.title('Charts')
 root.geometry("600x150")
 
-def zipLookup():
-    # zip.get()
-    # zipLabel = Label(root, text=zip.get())
-    # zipLabel.grid(row=1, column=0, columnspan=2)
-    
-    try:
-        api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+ zip.get()+"&distance=5&API_KEY=8B45E5B3-DACA-48C6-B6FC-36E275CE055E")
-        api = json.loads(api_request.content)
-        city = api[0]['ReportingArea']
-        quality = api[0]['AQI']
-        category = api[0]['Category']['Name']
 
-        if category == "Good":
-            weather_color = "#00e400"
-        elif category == "Moderate":
-            weather_color = "#ffff00"
-        elif category == "Unhealthy for Sensitive Groups":
-            weather_color = "#ff7e00"
-        elif category == "Unhealthy":
-            weather_color = "#ff0000"
-        elif category == "Very Unhealthy":
-            weather_color = "#8f3f97"
-        elif category == "Hazardous":
-            weather_color = "#7e0023"
-    
-
-        root.configure(background=weather_color)
-        myLabel = Label(root, text=city + " " + "Air Quality " + str(quality) + " " + category, font=("Helvetiva", 20), background=weather_color)
-        myLabel.grid(row=0, column=0)
-    except Exception as e:
-        api = "Errorr.."
+def graph():
+    house_prices = np.random.normal(200, 250, 500)
+    plt.polar(house_prices)
+    plt.show()
 
 
-
-
-
-
-zip = Entry(root)
-zip.grid(row=1, column=0, stick = W+E+N+S)
-
-zipButton = Button(root, text="Lookup Zipcode", command=zipLookup)
-zipButton.grid(row=1, column=1, stick = W+E+N+S)
-
+my_button = Button(root, text="Graph It!", command=graph)
+my_button.pack()
 
 root.mainloop()
 
